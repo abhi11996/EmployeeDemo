5cdbf696e3afd6083fcb553bde0f830e089a87e2
abhi11996 <abhimanyughadi362@gmail.com>
2021-12-09 10:47:25 +0530

    employee added

---
+++ EmployeeWebApiwithPodstest/Controllers/EmployeeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace EmployeeWebApiwithPodstest.Controllers
+{
+//    [System.Web.Http.Route("api/[controller]")]
+//    [ApiController]
+    public class EmployeeController : System.Web.Http.ApiController
+    {
+        static readonly IEmployeeRepository repository = new EmployeeRepository();
+
+        [System.Web.Http.HttpGet]
+        public IEnumerable<Employee> GetAllEmployee()
+        {
+            return repository.GetAll();
+        }
+        public Employee GetEmployee(int id)
+        {
+            Employee item = repository.Get(id);
+            if (item == null)
+            {
+                throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.NotFound));
+            }
+            return item;
+        }
+        public IEnumerable<Employee> GetEmployeeByName(string name)
+        {
+            return repository.GetAll().Where(
+                p => string.Equals(p.Name, name, StringComparison.OrdinalIgnoreCase));
+        }
+        public HttpResponseMessage PostEmployee(Employee item)
+        {
+            item = repository.Add(item);
+            var response = Request.CreateResponse<Employee>(HttpStatusCode.Created, item);
+            string uri = Url.Link("DefaultApi", new { id = item.Id });
+            response.Headers.Location = new Uri(uri);
+            return response;
+        }
+        public void PutEmployee(int id, Employee employee)
+        {
+            employee.Id = id;
+            if (!repository.Update(employee))
+            {
+                throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.NotFound));
+            }
+        }
+        public HttpResponseMessage DeleteEmployee(int id)
+        {
+            repository.Remove(id);
+            return new HttpResponseMessage(HttpStatusCode.NoContent);
+        }
+    }
+}
